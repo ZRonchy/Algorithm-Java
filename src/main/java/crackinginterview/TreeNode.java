2b2657878ff5c7289b72670cd5151de9cd03c973
chi.zhang <chi.zhang@ocado.com>
2017-11-20 23:25:06 +0000

    Create binary tree with minimum height

---
+++ src/main/java/crackinginterview/TreeNode.java
@@ -0,0 +1,16 @@
+package crackinginterview;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+
+    @Override
+    public String toString() {
+        return val + "";
+    }
+}
