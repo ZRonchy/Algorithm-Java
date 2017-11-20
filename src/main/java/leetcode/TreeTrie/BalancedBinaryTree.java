69a3936fc0204da9baa879c727a4fe6dc8ef9442
chi.zhang <chi.zhang@ocado.com>
2017-11-20 23:00:12 +0000

    Second solution to check if a tree is balanced

--- src/main/java/leetcode/TreeTrie/BalancedBinaryTree.java
+++ src/main/java/leetcode/TreeTrie/BalancedBinaryTree.java
@@ -5,10 +5,7 @@ package leetcode.TreeTrie;
 // a binary tree in which the depth of the two subtrees of every node
 // never differ by more than 1.
 public class BalancedBinaryTree {
-    public static void main(String args[]) {
-
-    }
-
+    /*Solution 1*/
     public boolean isBalanced(TreeNode root) {
         if (root == null)
             return true;
@@ -36,4 +33,29 @@ public class BalancedBinaryTree {
         return Math.max(left, right) + 1;
 
     }
+
+    /**
+     * Solution 2
+     * the difference of min depth and max depth should not exceed 1,
+     * since the difference of the min and the max depth is the maximum distance difference
+     * possible in the tree.
+    */
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static  int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1+Math.min(minDepth(root.left), minDepth(root.right));
+    }
+
+    public static boolean isBalanced1(TreeNode root) {
+        return (maxDepth(root) - minDepth(root) <= 1);
+    }
+
 }
