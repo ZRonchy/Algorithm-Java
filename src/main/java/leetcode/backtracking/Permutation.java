b22765081846f2af3c6fbf823b716b750b12d6d8
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-09-06 21:56:02 +0100

    Permutation on duplicates

--- src/main/java/leetcode/backtracking/Permutation.java
+++ src/main/java/leetcode/backtracking/Permutation.java
@@ -1,6 +1,7 @@
 package leetcode.backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,6 +22,7 @@ import java.util.List;
  ]
  */
 public class Permutation {
+    /*No duplicates*/
     public List<List<Integer>> permute(int[] nums) {
         boolean[] used = new boolean[nums.length];
         List<List<Integer>> result = new LinkedList<List<Integer>>();
@@ -39,6 +41,31 @@ public class Permutation {
                 permute(nums, used, temp, result);
                 temp.remove(temp.size() - 1);
                 used[i] = false;
+            }
+        }
+    }
+    /*If contains duplicates*/
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+        permuteUnique(nums, used, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private void permuteUnique(int[] nums, boolean[] used,
+                               List<Integer> temp, List<List<Integer>> result){
+        if(temp.size() == nums.length)
+            result.add(new ArrayList<>(temp));
+        else{
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || (i > 0 && nums[i] == nums[i-1] && !used[i-1]))
+                    continue;
+                used[i] = true;
+                temp.add(nums[i]);
+                permuteUnique(nums, used, temp, result);
+                used[i] = false;
+                temp.remove(temp.size() - 1);
             }
         }
     }
