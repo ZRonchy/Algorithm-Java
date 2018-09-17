b16321585d08de1d6c4e6d0d7c6873e443d16b55
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-09-17 22:20:35 +0100

    Subset problem dp soluion

--- src/main/java/leetcode/dynamicprogramming/SubsetProblem.java
+++ src/main/java/leetcode/dynamicprogramming/SubsetProblem.java
@@ -28,6 +28,31 @@ public class SubsetProblem {
                 isSubsetSum(set, n - 1, sum - set[n - 1]);
     }
 
+    static boolean isSubsetSum_dp(int set[], int n, int sum) {
+        // The value of subset[i][j] will be true if there is a subset of
+        // set[0..j-1] with sum equal to i
+        boolean subset[][] = new boolean[sum + 1][n + 1];
+        // If sum is 0, then answer is true
+        for (int i = 0; i <= n; i++) {
+            subset[0][i] = true;
+        }
+
+        // If sum is not 0 and set is empty, then answer is false
+        for (int i = 1; i <= sum; i++) {
+            subset[i][0] = false;
+        }
+
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= n; j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set[j - 1]) {
+                    subset[i][j] = subset[i][j] || subset[i - set[j - 1]][j - 1];
+                }
+            }
+        }
+        return subset[sum][n];
+    }
+
     /* Driver program to test above function */
     public static void main(String args[]) {
         int set[] = {3, 34, 4, 12, 5, 2};
