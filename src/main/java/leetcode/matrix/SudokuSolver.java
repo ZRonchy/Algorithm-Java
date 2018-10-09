d18c6e675599c2dbbb6d6c604263ac56cfe0404b
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-10-09 22:44:00 +0100

    Update Sudoku

--- src/main/java/leetcode/matrix/SudokuSolver.java
+++ src/main/java/leetcode/matrix/SudokuSolver.java
@@ -7,66 +7,49 @@ import java.util.HashSet;
  */
 public class SudokuSolver {
     public void solveSudoku(char[][] board) {
-        solve(board);
+        if (board==null||board.length==0)
+            return;
+        helper(board);
     }
 
-    public boolean solve(char[][] board){
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                if(board[i][j]!='.')
-                    continue;
+    private boolean helper(char[][] board){
+        for(int i=0; i<board.length; i++){
+            for (int j=0; j<board[0].length; j++){
+                if (board[i][j]=='.'){
+                    for (char num='1'; num<='9'; num++){//尝试
+                        if(isValid(board, i, j, num)){
+                            board[i][j]=num;
 
-                for(int k=1; k<=9; k++){
-                    board[i][j] = (char) (k+'0');
-                    if(isValid(board, i, j) && solve(board))
-                        return true;
-                    board[i][j] = '.';
+                            if (helper(board))
+                                return true;
+                            else
+                                board[i][j]='.';//回退
+                        }
+                    }
+                    return false;
                 }
-
-                return false;
             }
         }
 
         return true;
     }
 
-    public boolean isValid(char[][] board, int i, int j){
-        HashSet<Character> set = new HashSet<Character>();
-
-        for(int k=0; k<9; k++){
-            if(set.contains(board[i][k]))
+    private boolean isValid(char[][] board, int i, int j, char c){
+        // check column
+        for (int row=0; row<9; row++)
+            if (board[row][j] == c)
                 return false;
 
-            if(board[i][k]!='.' ){
-                set.add(board[i][k]);
-            }
-        }
-
-        set.clear();
-
-        for(int k=0; k<9; k++){
-            if(set.contains(board[k][j]))
+        // check row
+        for (int col=0; col<9; col++)
+            if (board[i][col]==c)
                 return false;
 
-            if(board[k][j]!='.' ){
-                set.add(board[k][j]);
-            }
-        }
-
-        set.clear();
-
-        for(int m=0; m<3; m++){
-            for(int n=0; n<3; n++){
-                int x=i/3*3+m;
-                int y=j/3*3+n;
-                if(set.contains(board[x][y]))
+        // check block
+        for(int row=i/3*3; row<i/3*3+3; row++)
+            for (int col=j/3*3; col<j/3*3+3; col++)
+                if (board[row][col]==c)
                     return false;
-
-                if(board[x][y]!='.'){
-                    set.add(board[x][y]);
-                }
-            }
-        }
 
         return true;
     }
