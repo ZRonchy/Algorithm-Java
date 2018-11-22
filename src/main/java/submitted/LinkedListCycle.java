b112d9359d8a0578209f813ab63503a302e4e3d4
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-11-22 21:38:36 +0000

    LinkedList Cycle

---
+++ src/main/java/submitted/LinkedListCycle.java
@@ -0,0 +1,26 @@
+package submitted;
+
+/**
+ * https://leetcode.com/submissions/detail/191137100/
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
