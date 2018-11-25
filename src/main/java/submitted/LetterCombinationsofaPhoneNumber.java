78018839651b35b1ae22430e49c931c7acf90cc2
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-11-25 23:09:18 +0000

    Letter Combinations of a Phone Number

---
+++ src/main/java/submitted/LetterCombinationsofaPhoneNumber.java
@@ -0,0 +1,33 @@
+package submitted;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/submissions/detail/191688020/
+ * time : O(3^n)
+ * space : O(n)
+ */
+public class LetterCombinationsofaPhoneNumber {
+    private String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        helper(res, digits, "", 0);
+        return res;
+    }
+
+    public void helper(List<String> res, String digits, String s, int index) {
+        if (index == digits.length()) {
+            res.add(s);
+            return;
+        }
+        String letters = mapping[digits.charAt(index) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            helper(res, digits, s + letters.charAt(i), index + 1);
+        }
+    }
+}
--- src/main/java/submitted/SearchA2DMatrixII.java
+++ src/main/java/submitted/SearchA2DMatrixII.java
@@ -3,24 +3,26 @@ package submitted;
 /**
  * https://leetcode.com/submissions/detail/191677895/
  * Binary Search from (m,0)
+ * time : O(m + n)
+ * space : O(1)
  */
 public class SearchA2DMatrixII {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
-        int m=matrix.length-1;
-        int n=matrix[0].length-1;
+        int m = matrix.length - 1;
+        int n = matrix[0].length - 1;
 
-        int i=m;
-        int j=0;
+        int i = m;
+        int j = 0;
 
-        while(i>=0 && j<=n){
-            if(target < matrix[i][j]){
+        while (i >= 0 && j <= n) {
+            if (target < matrix[i][j]) {
                 i--;
-            }else if(target > matrix[i][j]){
+            } else if (target > matrix[i][j]) {
                 j++;
-            }else{
+            } else {
                 return true;
             }
         }
