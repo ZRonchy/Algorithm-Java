5033478bf15f52062069a2e699977ce91875fa17
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-11-26 23:28:33 +0000

    Letter Combinations of a Phone Number

--- src/main/java/submitted/LetterCombinationsofaPhoneNumber.java
+++ src/main/java/submitted/LetterCombinationsofaPhoneNumber.java
@@ -1,6 +1,7 @@
 package submitted;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,5 +30,24 @@ public class LetterCombinationsofaPhoneNumber {
         for (int i = 0; i < letters.length(); i++) {
             helper(res, digits, s + letters.charAt(i), index + 1);
         }
+    }
+
+    public List<String> letterCombinations2(String digits) {
+        LinkedList<String> res = new LinkedList<>();
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        res.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            int num = digits.charAt(i) - '0';
+            while (res.peek().length() == i) {
+                String t = res.remove();
+                for (char s : mapping[num].toCharArray()) {
+                    res.add(t + s);
+                }
+            }
+        }
+        return res;
     }
 }
