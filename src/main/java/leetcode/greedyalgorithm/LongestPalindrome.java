92845d81d76f05f51559b7d5f8bf387818f50398
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-12-26 19:23:28 +0000

    Word Frequency

--- src/main/java/leetcode/greedyalgorithm/LongestPalindrome.java
+++ src/main/java/leetcode/greedyalgorithm/LongestPalindrome.java
@@ -26,6 +26,17 @@ package leetcode.greedyalgorithm;
  */
 public class LongestPalindrome {
     public int longestPalindrome(String s) {
+        boolean[] map = new boolean[128];
+        int len = 0;
+        for (char c : s.toCharArray()) {
+            map[c] = !map[c];         // flip on each occurrence, false when seen n*2 times
+            if (!map[c]) len+=2;
+        }
+        if (len < s.length()) len++; // if more than len, atleast one single is present
+        return len;
+    }
+
+    public int longestPalindrome2(String s) {
         int[] cnt = new int[58];
         int max = 0;
         int len = 0;
