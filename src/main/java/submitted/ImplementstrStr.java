d83a5571a6f4d9e3a1780d7355d6d591a79c9aa1
chi.zhang <chi.ronchy.zhang@gmail.com>
2018-12-29 03:09:31 +0000

    Implement strStr

---
+++ src/main/java/submitted/ImplementstrStr.java
@@ -0,0 +1,22 @@
+package submitted;
+
+public class ImplementstrStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) return i;
+        }
+        return -1;
+    }
+
+    public int strStr2(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (i + j == haystack.length()) return -1;
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+    }
+}
