e92652c1e1420527b91568e05b84a00f3f2b58ea
chi.zhang <chi.ronchy.zhang@gmail.com>
2019-01-01 17:40:34 +0000

    Word Ladder II

--- src/main/java/submitted/WordLadder.java
+++ src/main/java/submitted/WordLadder.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 /**
  * https://leetcode.com/submissions/detail/192482337/
- * time: O(n)
+ * time: O(n*26^l) -> O(n*26^l/2), l = len(word)
  * time: O(n)
  */
 public class WordLadder {
