202fbac85b198e061ea7a59a13a47bbc98b02be0
chi.zhang <chi.ronchy.zhang@gmail.com>
2019-01-19 01:20:27 +0000

    Min Cost Climbing Stairs

--- src/main/java/submitted/MinCostClimbingStairs.java
+++ src/main/java/submitted/MinCostClimbingStairs.java
@@ -9,7 +9,29 @@ package submitted;
  *    answer = min(dp[n-1], dp[n-2])
  */
 public class MinCostClimbingStairs {
+    // time O(n) space O(n)
     public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length+1];
+        for (int i = 2; i <= cost.length; ++i) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1],
+                    dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.length];
+    }
+
+    // time O(n) space O(1)
+    public int minCostClimbingStairs1(int[] cost) {
+        int dp1 = 0;
+        int dp2 = 0;
+        for (int i = 2; i <= cost.length; ++i) {
+            int dp = Math.min(dp1 + cost[i - 1], dp2 + cost[i - 2]);
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp1;
+    }
+
+    public int minCostClimbingStairs2(int[] cost) {
         for(int i = 2; i < cost.length; i++) {
             cost[i] += Math.min(cost[i-1], cost[i-2]);
         }
