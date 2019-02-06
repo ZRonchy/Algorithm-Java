697fc55ba4977b1c80c4c15f8f9ef627cec2c325
chi.zhang <chi.ronchy.zhang@gmail.com>
2019-02-06 01:27:43 +0000

    Reverse Second Half Of Linked List

---
+++ src/main/java/submitted/ReverseSecondHalfOfLinkedList.java
@@ -0,0 +1,28 @@
+package submitted;
+
+public class ReverseSecondHalfOfLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        //因为中间也需要变，所以要添加dummy
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //找到中间的位置了slow 右侧开始反转
+        ListNode pre = null;
+        ListNode cur = slow.next;
+
+        while(cur != null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        slow.next = pre;
+        return head;
+    }
+}
