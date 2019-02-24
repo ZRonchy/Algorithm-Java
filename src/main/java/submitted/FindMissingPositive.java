5ca8332b09de676869628f7181e08dc71c2fb239
chi.zhang <chi.ronchy.zhang@gmail.com>
2019-02-24 23:11:42 +0000

    Find Missing Positive

---
+++ src/main/java/submitted/FindMissingPositive.java
@@ -0,0 +1,30 @@
+package submitted;
+
+/**
+ * https://leetcode.com/submissions/detail/210369917/
+ * time : O(n)
+ * space : O(1)
+ */
+public class FindMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // everytime we put one number at the correct position, so O(n) in total
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                // nums[i] - 1 is where nums[i] should be put, if the number there is not nums[i],
+                // swap nums[i] to the correct positin nums[i]-1, bucket sort idea
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
