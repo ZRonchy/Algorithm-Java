08618ca19421278b53e227613dc7d6a637b1daf9
chi.zhang <chi.ronchy.zhang@gmail.com>
2019-05-06 21:38:50 +0000

    Reverse K group

--- src/main/java/submitted/ReverseKGroup.java
+++ src/main/java/submitted/ReverseKGroup.java
@@ -1,29 +1,54 @@
 package submitted;
 
 /**
- * https://leetcode.com/submissions/detail/206005521/
+ * https://leetcode.com/submissions/detail/227211739/
  * time O(n)
  * space O(n)
  */
 public class ReverseKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || head.next == null) return head;
+        if(head == null) {
+            return null;
+        }
+
+        // find the first k group and isolate it from the rest
+        ListNode tempHead = head, trailer = null;
         int count = 0;
-        ListNode cur = head;
-        while (cur != null && count != k) {
-            cur = cur.next;
+        while(head != null && count < k){
+            trailer = head;
+            head = head.next;
             count++;
         }
-        if (count == k) {
-            cur = reverseKGroup(cur, k);
-            while (count-- > 0) {
-                ListNode temp = head.next;
-                head.next = cur;
-                cur = head;
-                head = temp;
-            }
-            head = cur;
+        if(head == null && count < k) {
+            return tempHead;
         }
-        return head;
+        trailer.next = null; // de-tail
+
+        // do a normal reverse of list
+        ListNode ret = reverseList(tempHead);
+
+        // tempHead becomes the tale to which we can append the rest
+        tempHead.next = reverseKGroup(head, k);
+
+        return ret;
     }
+
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode pre = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+
+        return pre;
+    }
+
 }
